7803de117e7ff67aba764bcf8caa335ec016cb14
Matija Osojnik <matija1.osojnik@gmail.com>
2020-02-07 21:59:00 +0100

    Created Register+Login Form, Created User Model, Added Authentication

--- server/app.js
+++ server/app.js
@@ -3,25 +3,72 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const jsdom = require("jsdom");
+
 const mongoose = require("mongoose");
-path = require("path")
+const Schema = mongoose.Schema;
+
+const cors = require("cors"); 
+const session = require("express-session");
+const handleError = require("errorhandler");
+const router = require("express-promise-router")
+
+const passport = require("passport")
+
+const assert = require("assert")
+
+path = require("path");
 $ = require("jquery")(new jsdom.JSDOM().window);
+
+mongoose.promise = global.Promise;
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 const app = express();
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 
+
+app.use(cors());
+app.use(require('morgan')('dev'))
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "../") + "/public"));
-
 app.set("views", path.join(__dirname, "../views"));
+app.use(session({secret: 'language-learning-site', cookie: {maxAge: 60000}, resave: false, saveUninitialized: false }));
+
 app.engine("html", require("ejs").renderFile);
 
 app.set("view engine", "ejs");
+//configure Mongoose
 
-const db = mongoose.connection;
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('connected to database'));
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true }).catch(error => handleError(error));
+mongoose.set('debug', true);
+
+
+// const admin = new Users({
+//   nickName: 'Admin',
+//   email: 'matijaosojnik@protonmail.com',
+//   password: 'root'
+// });
+
+// admin.save((err) => {
+//   if(err) return handleError(err);
+// });
+
+
+
+//expressjs - res.status is not a function TypeError - add next to the (err, req, res)
+//https://github.com/visionmedia/supertest/issues/416#
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+
+  res.json({
+    error: {
+      message: err.message,
+      error: {},
+    },
+  });
+});
 
 const learn = require(__dirname + "/routes/learn.js");
 const practice = require(__dirname + "/routes/practice.js");
@@ -32,28 +79,6 @@ app.use('/practice', practice);
 app.use('/learn', learn);
 app.use('/about', about);
 app.use('/', main);
-
-app.route("/login")
-.get((req,res) => {
-  res.render("login");
-})
-.post((req, res) => {
-  const name = req.body.email;
-  const surname = req.body.password;
-  console.log(name, surname)
-  res.redirect("/login")
-  })
-
-app.route("/register")
-  .get((req, res) => {
-    res.render("register");
-    })
-    .post((req, res) => {
-      const name = req.body.email;
-      const surname = req.body.password;
-      console.log(name, surname);
-      res.redirect("/login");
-    });
 
 app.listen(3000, ()=>{
   console.log("Server started on port 3000")
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const handleError = require("errorHandler")
 const router = express.Router();
 const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+let nickname = ""
+
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const app = express();
 
@@ -12,14 +19,92 @@ app.engine("html", require("ejs").renderFile);
 
 app.set("view engine", "html");
 
+const userSchema = new Schema({
+  nickName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  creationDate: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+const Users = mongoose.model('Users', userSchema);
+
 // middleware that is specific to this router
 // router.use(function timeLog(req, res, next) {
 //   console.log("Time: ", Date.now());
 //   next();
 // });
 // define the home page route
-router.get("/", (req, res) => {
-  res.render("main");
-});
+
+router.route("/register")
+  .get((req, res) => {
+    res.render("register");
+    })
+    .post((req, res) => {
+
+      bcrypt.hash(req.body.password, 10, (err, hash) => {
+
+        const user = new Users({
+          nickName: req.body.nickName,
+          email: req.body.email,
+          password: hash
+        })
+        user.save((err) => {
+          if(err) {
+            res.redirect("/register");
+            return handleError(err);
+          }else{
+            res.render("login")
+          }
+        })
+      })
+
+
+    }); 
+
+router.route("/login")
+.get((req,res) => {
+  res.render("login");
+})
+.post((req, res) => {  
+
+  
+  Users.findOne({email: req.body.email}, (err, foundUser) => {
+    nickname = foundUser.nickName;
+      if(err) return handleError(err);
+      else{
+
+
+          bcrypt.compare(req.body.password, foundUser.password).then(result => {
+               if(result){
+               res.render("main", {nickName: nickname})
+              }
+               else{
+                console.log("Wrong password");
+                 res.redirect("login")
+               }
+          }).catch((err) => console.log(err))         
+      }
+    });
+  
+  })
+
+  router.get("/", (req, res) => {
+
+    res.render("main", {nickName: nickname});
+  });
+
+
 
 module.exports = router;
